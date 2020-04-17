17ca50f928df49e157ae0a5ecc9dbbb7d6019b54
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-04-17 14:24:44 +0200

    I001D: Kang coral LMKD config
    * This also enabled PSI in lmkd since kernel supports it
    
    Change-Id: I502d654cc96e459eb5e1dbb0062bf06b7e0deeb0

--- system_prop.mk
+++ system_prop.mk
@@ -80,14 +80,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # LMKD
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lmk.use_minfree_levels=true \
-    ro.lmk.use_psi=false \
-    ro.lmk.low=1001 \
-    ro.lmk.medium=800 \
-    ro.lmk.critical=200 \
+    ro.config.low_ram=false \
     ro.lmk.kill_heaviest_task=true \
-    ro.lmk.upgrade_pressure=100 \
-    ro.lmk.downgrade_pressure=100
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true \
+    ro.lmk.log_stats=true
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
